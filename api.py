77215ada4941e6d6a87a39bdc9e527c2c9b1b8a1
Cristi Stanescu <astanescu96@yahoo.com>
2025-05-10 19:47:48 +0300

    options headers validations

--- api.py
+++ api.py
@@ -37,7 +37,7 @@ def add_cors_headers(response):
         response.headers["Access-Control-Allow-Origin"] = origin
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization, Client-Id"
     return response
 
 @app.route('/ask', methods=['GET'])
